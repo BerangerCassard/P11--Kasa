b6ba24fe07810ea3cac10bbb597f15e2414f24c4
bronicles <72601426+bronicles@users.noreply.github.com>
2022-01-04 16:27:53 +0100

    header nav

---
+++ src/components/navigation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import '../styles/navigation.css'
+import logo from '../assets/img/logo.png'
+
+class Navigation extends React.Component{
+
+render() {
+
+	return (
+		<nav className='header'>
+			<div>
+				<img src={logo} alt='logo'/>
+			</div>
+
+			<ul className='menu'>
+				<li>
+					Accueil
+				</li>
+				<li>
+					A Propos
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+}
+
+export default Navigation
